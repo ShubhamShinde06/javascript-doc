24ebc4b48764bf70be044f8d47bd14ad877144e2
ShubhamShinde06 <officialshubhamorg@gmail.com>
2024-08-06 20:43:03 +0530

    variables

---
+++ 01_basci/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553 // a keyword introduced for declaring variables in JavaScript
+let accountEmail = "shubham@gmail.com" // a keyword introduced in ES6 for declaring variables in JavaScript
+var accountPassword = "12345" // prefer not to use var because of issue in block scope and functional scope
+accountCity = "jaipur" // javascript possiable to not define keyword and and data in variable
+let accountData; // in console show undefined
+
+//accountId = 2 //not allowed in const
+accountEmail = "hc@hmail.in"
+accountPassword = "1234"
+accountCity = "mumbai"
+
+console.log([accountEmail,accountPassword,accountCity,accountData])
