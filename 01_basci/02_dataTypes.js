d4b93a28c2d4077c6a42cd349a745708bb2fa98d
ShubhamShinde06 <officialshubhamorg@gmail.com>
2024-08-06 21:03:26 +0530

    datatypes

---
+++ 01_basci/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) // we re useing nodejs, not a browser
+
+// number => 2 to power 53
+// bigint => big amount of numbers
+// string => "" / ''
+// boolean = true/false
+// null => standlone value
+// undefined = > 
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
