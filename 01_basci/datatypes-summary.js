c17a22d02d44c8a5b8ae8d734bb477d1e9f0913f
ShubhamShinde06 <officialshubhamorg@gmail.com>
2024-08-09 21:54:55 +0530

    datatype-summary

---
+++ 01_basci/datatypes-summary.js
@@ -0,0 +1,63 @@
+//JavaScript is a dynamic language and not static,
+
+// Primitive (callback value, variable direct give an copy )
+/*
+    1.sting
+    2.number
+    3.boolean
+    4.null
+    5.undefine
+    6.symbol
+    7.bigint
+*/
+
+// Reference (non primitive, in memory direct refrence give you)
+/*
+    1.array
+    const heros = ["shaktiman","kiresh","benten"]
+
+    2.objects
+    {
+        name:"shubham",
+        age:15
+    },
+    {
+        name:"joan"
+        age:67
+    }
+
+
+    3.function
+    function(){
+        console.log("hellow world!!!")
+    }
+*/
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive)
+/*
+    let drive1 = "shubham_01"
+    let drive2 = drive1
+
+    drive2 = "shubham_02"
+    console.log(drive1)
+    console.log(drive2)   
+*/
+
+// Heap (Non-Primitive)
+/*
+    let userOne = {
+        email:"use@gmail.com"
+    }
+    let userTwo = userOne
+
+    userTwo.email = "use2@gmail.com"
+    console.log(userOne.email)
+    console.log(userTwo.email)
+*/
+
+
+
+
