09d13eb1e1a075f0c60b64ad4b30175f5b162ecb
ShubhamShinde06 <officialshubhamorg@gmail.com>
2024-08-12 21:35:08 +0530

    strings

---
+++ 01_basci/05_string.js
@@ -0,0 +1,31 @@
+const name = "shubham"
+const repoCount = 50
+
+//console.log(name + repoCount + " Done") //old
+
+//console.log(`hello my name is ${name} and my repo count ${repoCount}`) // new
+
+const gameName = new String('shubhamFC')
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('u'))
+
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-7, 4)
+// console.log(anotherString)
+
+// const newString1 = "  shubham    "
+// console.log(newString1.trim())
+
+// const url = "https://google.com/shubham%20"
+// console.log(url.replace('%20', 'yes'))
+// console.log(url.includes('shubham'))
+
+
+// const string = new String('shubhamFC-url-but')
+// console.log(string.split('-'))
