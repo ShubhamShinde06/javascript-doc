12731db0a16c3ae10abc4835b6153950c92127d1
ShubhamShinde06 <officialshubhamorg@gmail.com>
2024-08-15 19:53:21 +0530

    array part one

---
+++ 02_Basci/01_array.js
@@ -0,0 +1,41 @@
+// arrays ----------------------------
+// javascript arrays are resizable and can containa mix of diierent data types
+// shallow copy (of an object a copy whose properties share the same references)
+// deap copy (do not share the same references)
+
+const myArr = [0, 1, 2, 3, 4]
+const myHeros = ["shaktiman","kiresh"]
+
+const myArr2 = new Array(1,2,3,4)
+//console.log(myArr[0]);
+
+// array methods ----------------------
+
+//myArr.push(6) // put
+//myArr.pop() // delete
+
+//myArr.unshift(9) // this value add index(0)
+//myArr.unshift() // remove the index(0) value
+
+//console.log(myArr.includes(9)); // this value not in array (false)
+//console.log(myArr.indexOf(3)); // index value show
+
+//const newArr = myArr.join() // array show but in string fromate
+
+//console.log(myArr)
+//console.log(newArr);
+
+// slice, splice ---------------------------------------------
+
+console.log("A ", myArr) // orignal array
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1) // 1 - 3 last index ex:- 3 not include
+console.log("B", myArr)
+
+
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2); // index (0 - 5) 1 to 3 num cut and show
+console.log("C", myArr)
+
